aff93e69de20fd8e6fc49f9388dd73278708355b
EuniceKiang <eunice.kiang@yale.edu>
2023-05-10 15:05:37 -0400

    Lighting of the sun

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -7,13 +7,13 @@ class BasicLights extends Group {
 
         // Simulating the front facing sun
         const sun1 = new SpotLight(0xffffff, 2, 100, 2, 1, 1);
-        sun1.position.set(15, 50, 0);
+        sun1.position.set(-15, 50, -15);
         sun1.target.position.set(-7, 0, 0);
         // const sun2 = new SpotLight().copy(sun1);
         // sun2.position.set(10, 50, -5);
 
         // sunset beach color
-        const ambi = new AmbientLight(0xfbdc5c, 1.32); 
+        const ambi = new AmbientLight(0xfb8a4c, 1.32);  // 0x + hexcode
 
         // No need for this
         // const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
