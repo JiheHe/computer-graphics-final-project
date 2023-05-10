e9ad4966ffcc498bc1991120868a025b6e924adb
JiheHe <nickhe2003@gmail.com>
2023-05-10 14:33:40 -0400

    Sunset beach lighitng

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -5,14 +5,21 @@ class BasicLights extends Group {
         // Invoke parent Group() constructor with our args
         super(...args);
 
-        const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
-        const ambi = new AmbientLight(0x404040, 1.32);
-        const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
+        // Simulating the front facing sun
+        const sun1 = new SpotLight(0xffffff, 2, 100, 2, 1, 1);
+        sun1.position.set(15, 50, 0);
+        sun1.target.position.set(-7, 0, 0);
+        // const sun2 = new SpotLight().copy(sun1);
+        // sun2.position.set(10, 50, -5);
 
-        dir.position.set(5, 1, 2);
-        dir.target.position.set(0, 0, 0);
+        // sunset beach color
+        const ambi = new AmbientLight(0xfbdc5c, 1.32); 
 
-        this.add(ambi, hemi, dir);
+        // No need for this
+        // const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
+        // hemi.position.set(0, -0.5, 0);
+
+        this.add(ambi, sun1);
     }
 }
 
